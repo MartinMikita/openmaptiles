cc9a64c465fa4c6ab851451034c25560d6e93f48
MartinMikita <martin@mikita.eu>
2018-07-27 12:34:12 +0200

    Updated skiing layer - simpler SQL and mapping.

--- layers/skiing/layer.sql
+++ layers/skiing/layer.sql
@@ -1,12 +1,7 @@
--- etldoc: layer_skiing[shape=record fillcolor=lightpink, style="rounded,filled",
--- etldoc:     label="layer_skiing |<z12> z12|<z13> z13|<z14_> z14+" ];
 
 CREATE OR REPLACE FUNCTION layer_skiing(bbox geometry, zoom_level int)
-RETURNS TABLE(geometry geometry, class text, name text, occupancy int, capacity int, duration int) AS $$
-    SELECT geometry, aerialway AS class, name, occupancy, capacity, duration FROM (
-        -- etldoc:  osm_skiing_aerial_linestring -> layer_skiing:z10_
-        SELECT geometry, aerialway, name, occupancy, capacity, duration
-        FROM osm_skiing_aerial_linestring WHERE zoom_level >= 10
-    ) AS zoom_levels
-    WHERE geometry && bbox;
+RETURNS TABLE(geometry geometry, class text, name text) AS $$
+    SELECT geometry, class, name
+    FROM osm_skiing_linestring
+    WHERE zoom_level >= 10 AND geometry && bbox;
 $$ LANGUAGE SQL IMMUTABLE;
